50ba3acaf71eb616173e51dea1694f9d9db1062f
Gian Akbar <akbarigi20@gmail.com>
2025-08-28 19:26:09 +0700

    fix: csp

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export const dynamic = 'force-static';
-export const revalidate = 60 * 60 * 24; // 24h
+export const revalidate = 86400; // 24h
 
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://sengiku.studio';
